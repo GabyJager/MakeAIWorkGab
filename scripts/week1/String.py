00be124e0f0c490920b01ed1bee85aea8d111be6
GabyJager <0870145@student.hr.nl>
2023-02-08 15:55:49 +0100

    x

---
+++ scripts/week1/String.py
@@ -0,0 +1,21 @@
+
+
+planet = 'Mercurius'
+star = 'Sun'
+myHomePlanet = 'Earth'
+
+
+formatOperatorStr = 'Since %s is so much closer to the %s than we are, in the Mercurian sky the %s\
+appear three times as big as from here on %s' %(planet,star,star,myHomePlanet)
+
+print (formatOperatorStr)
+
+
+object = "kbo"
+distanceUnit = "au"
+star = "SUN"
+
+formatOperatorStr = "So far, %s have been seen from distances ranging from 30%s to 50%s from the %s (for reference, Pluto's\ average orbit is at about 39%s." %(object.upper(), distanceUnit.upper(), distanceUnit.upper(), star.title(), distanceUnit.upper())
+
+print (formatOperatorStr)
+
