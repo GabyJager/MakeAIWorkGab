5a17a75fb361bf0d9584f2a050472d12456bae37
GabyJager <0870145@student.hr.nl>
2023-02-10 11:25:53 +0100

    add week1

---
+++ program/# program​.py
@@ -0,0 +1,13 @@
+print ('Please enter a word')
+
+word = input()
+counter = 0
+
+for char in word:
+
+    if char == 'a':
+
+        counter = counter + 1
+
+print (f'Paard contains {counter} vowels')
+
