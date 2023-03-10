eca6275468357324464ac671ee73ad19fcdf5f96
GabyJager <0870145@student.hr.nl>
2023-03-10 16:05:12 +0100

    add pipeline

---
+++ projects/Project/pipeline_voorbeeld_basis.py
@@ -0,0 +1,31 @@
+# Installing libraries
+import logging
+import sqlite3
+import pandas as pd
+import os
+
+# Global variables
+dbPath = "project"
+
+# Collecting the data
+# Specify the con
+con = sqlite3.connect(f'{dbPath}/db.sqlite3.')
+
+# Convert to DataFrame
+df = pd.read_sql('SELECT * FROM rest_api_netlify', con=con)
+
+
+
+
+
+# transform
+
+
+
+
+
+# Save df as new table
+df.to_sql('new_table', con=con, index=False)
+
+# close Connection
+con.close()
