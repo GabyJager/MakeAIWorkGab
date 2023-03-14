32aae65ab8681676b0b9996da35416113938849d
GabyJager <0870145@student.hr.nl>
2023-03-14 16:37:01 +0100

    add app, pipeline

--- projects/Project/Data/Pipeline_select.py
+++ projects/Project/Data/Pipeline_select.py
@@ -26,6 +26,27 @@ dfCleanFromDB = dfFromDB.dropna()
 logging.debug(dfCleanFromDB.head())
 
 # Transform
+
+for x in df.index:
+  if df.loc[x, "lifespan"] < 0:
+    df.drop(x, inplace = True)
+
+df["lifespan"]>1
+df.describe()
+
+duplicate_rows_df = df[df.duplicated()]
+print("number of duplicate rows: ", duplicate_rows_df.shape)
+df = df.drop_duplicates()
+
+print(df.isnull().sum())
+df = df.dropna()
+df.head()
+
+logging.info("Preprocessing : remove rows with missing values")
+dfCleanFromDB = dfFromDB.dropna()
+logging.debug(dfCleanFromDB.head())
+
+# Transform
 dfSelection = dfCleanFromDB[['length', 'mass', 'lifespan']]
 length = dfSelection['length']
 mass = dfSelection['mass']
@@ -36,6 +57,20 @@ noemer = pow(length/100, 2)
 bmi = (mass / noemer) if (noemer > 0) else 0
 logging.debug(f"BMI : {bmi}")
 
+df["bmi"] = round(df["mass"]/(df["length"]/100)**2, 2)
+
+df = df.drop(['length', 'mass'], axis=1)
+df.head()
+
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+IQR = Q3 - Q1
+print(Q1)
+print()
+print(Q3)
+print()
+print(IQR)
+df = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
 
 # Save df as new table
 dfSelection.to_sql('bmi', con=dbConnection, if_exists='replace', index=False)
